b9e1ee9ffa6a20c6ad66728073629b23db8bc547
Onc Bilisim <oncbilisim@gmail.com>
2021-08-26 15:59:42 +0300

    first commit

---
+++ controllers/MesajController.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\controllers;
+
+use yii\web\Controller as WebController;
+
+class MesajController extends WebController
+{
+
+    public function actionSoyle($kelime = 'Merhaba')
+    {
+        return  $this->render('soyle',['kelime' => $kelime]);
+    }
+
+}
